0722c4d958c767f0cd78629a79fa3d45367ac9c5
Nithishkumars-2001 <nithishkumar70102@gmail.com>
2024-07-27 08:45:50 +0530

    Updated all file

---
+++ script.js
@@ -0,0 +1,13 @@
+
+//  select side navbar
+
+var side_navbar = document.querySelector(".side-navbar")
+
+function showsidenavbar()
+{
+    side_navbar.style.left = "0"
+}
+function closesidenavbar()
+{
+    side_navbar.style.left = "-70%"
+}
